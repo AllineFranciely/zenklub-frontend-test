40b05ffeeb6ad53efecc3264583b84fb23916043
Alline Franciely <alline_franciely@hotmail.com>
2024-12-03 15:00:07 -0300

    Ajusta test do arquivo App.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock("axios");
+
+test("renders App component without crashing", () => {
+  const { container } = render(<App />);
+  expect(container).toBeInTheDocument();
 });
